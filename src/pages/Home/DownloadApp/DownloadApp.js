8303a30c3111682ca968f42fe52d853445298e1d
dipdastoko <dipdastoko@gmail.com>
2022-07-27 22:20:36 +0600

    download app section added

--- src/pages/Home/DownloadApp/DownloadApp.js
+++ src/pages/Home/DownloadApp/DownloadApp.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const DownloadApp = () => {
     return (
-        <div className='border border-primary m-2'>
+        <div className='mt-5'>
             <h2>Download Our App</h2>
+            <span><i>Download app app get your discount coupon for first time</i></span>
+            <br />
             <Link to='#'>
                 <img style={{ height: '100px' }} src="https://i.ibb.co/742QSFh/apple-app-store.png" alt="" />
             </Link>
-            &nbsp;
-            &nbsp;
-            &nbsp;
+            &nbsp;&nbsp;&nbsp;
             <Link to='#'>
                 <img style={{ height: '135px' }} src="https://i.ibb.co/MCrGgpp/google-play-store.png" alt="" />
             </Link>
