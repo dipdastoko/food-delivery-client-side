2e8b7d5a935a597c526c81f6bdeb412e85733d51
dipdastoko <dipdastoko@gmail.com>
2022-07-28 16:21:17 +0600

    header updated

--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -21,7 +21,15 @@ const Header = () => {
                             <NavLink to='/home' className='my-1'>Home</NavLink>
                             {!user.email ? <NavLink className='mx-4 my-1' to='/login'>Login</NavLink>
                                 :
-                                <NavLink className='mx-4 my-1' to='/'><button onClick={logOut} className='btn btn-primary'>Log Out</button></NavLink>
+                                <>
+                                    <NavLink className='mx-4 my-1' to='/login'>My Orders</NavLink>
+
+                                    <NavLink className='mx-4 my-1' to='/login'>Manage All Orders</NavLink>
+
+                                    <NavLink className='mx-4 my-1' to='/login'>Add New Food</NavLink>
+
+                                    <NavLink className='mx-4 my-1' to='/'><button onClick={logOut} className='btn btn-primary'>Log Out</button></NavLink>
+                                </>
                             }
                         </Nav>
                     </Navbar.Collapse>
