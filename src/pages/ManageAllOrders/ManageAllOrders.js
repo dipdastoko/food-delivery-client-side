46d9f9b6c2c2de46d5110acd125a7612c83fffbb
dipdastoko <dipdastoko@gmail.com>
2022-07-30 19:44:05 +0600

    manage all orders updated

--- src/pages/ManageAllOrders/ManageAllOrders.js
+++ src/pages/ManageAllOrders/ManageAllOrders.js
@@ -9,13 +9,24 @@ const ManageAllOrders = () => {
             <h2>Manage All Orders</h2>
             {
                 allOrders.map(order =>
-                    <div key={order._id} className='border mx-5 py-3 my-4'>
-                        <div>
-                            <img style={{ height: '150px' }} src={order.img} alt="" />
-                            <h5>{order.name}</h5>
+                    <div className='d-flex justify-content-between border mx-5 p-5 my-4'>
+                        <div key={order._id} >
+                            <div>
+                                <img style={{ height: '150px' }} src={order.img} alt="" />
+                                <h5>{order.name}</h5>
+                            </div>
+                            <p>Price: {order.price}tk</p>
+                            <button className='btn btn-primary'>Cancel Order</button>
                         </div>
-                        <p>Price: {order.price}tk</p>
-                        <button className='btn btn-primary'>Cancel Order</button>
+                        <div>
+                            <h2>To</h2>
+                            <blockquote><u>Address</u>
+                                <p>{order.address}</p>
+                                <p>{order.address2}</p>
+                            </blockquote>
+                            <h6>Phone: {order.phone}</h6>
+                            <h6>Email: {order.email}</h6>
+                        </div>
                     </div>
                 )
             }
