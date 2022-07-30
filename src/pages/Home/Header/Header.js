536fa19e03d9d32a36d3d664760f89fa1e5dd137
dipdastoko <dipdastoko@gmail.com>
2022-07-31 02:32:00 +0600

    header text decoration updated

--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -10,23 +10,23 @@ const Header = () => {
     const { user, logOut } = firebase;
 
     return (
-        <div >
+        <div className='txt'>
             <img className='banner' src={banner} alt="" />
             <Navbar bg="light" expand="lg">
                 <Container >
-                    <Navbar.Brand href="#home">Healty Food</Navbar.Brand>
+                    <Navbar.Brand href="#">Healty Food</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <NavLink to='/home' className='my-1'>Home</NavLink>
-                            {!user.email ? <NavLink className='mx-4 my-1' to='/login'>Login</NavLink>
+                            <NavLink to='/home' className='mx-4 my-1 text-decoration-none'>Home</NavLink>
+                            {!user.email ? <NavLink className='text-decoration-none mx-4 my-1' to='/login'>Login</NavLink>
                                 :
                                 <>
-                                    <NavLink className='mx-4 my-1' to='/myOrders'>My Orders</NavLink>
+                                    <NavLink className='text-decoration-none mx-4 my-1' to='/myOrders'>My Orders</NavLink>
 
-                                    <NavLink className='mx-4 my-1' to='/allOrders'>Manage All Orders</NavLink>
+                                    <NavLink className='text-decoration-none mx-4 my-1' to='/allOrders'>Manage All Orders</NavLink>
 
-                                    <NavLink className='mx-4 my-1' to='/addNewFood'>Add New Food</NavLink>
+                                    <NavLink className='text-decoration-none mx-4 my-1' to='/addNewFood'>Add New Food</NavLink>
 
                                     <NavLink className='mx-4 my-1' to='/'><button onClick={logOut} className='btn btn-primary'>Log Out</button></NavLink>
                                 </>
